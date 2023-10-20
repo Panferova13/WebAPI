0b7f6562d9bbbc19f825ae6d16ff4116c871b940
A.Panferova <panferovaa@icloud.com>
2023-10-20 18:35:33 +0300

    lab4

---
+++ lrs/MappingProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using Entities.DataTransferObjects;
+using Entities.Models;
+
+namespace lrs
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Company, CompanyDto>()
+            .ForMember(c => c.FullAddress,
+            opt => opt.MapFrom(x => string.Join(' ', x.Address, x.Country)));
+            CreateMap<Employee, EmployeeDto>();
+            CreateMap<City, CityDto>();
+            CreateMap<Hotel, HotelDto>()
+        }
+    }
+}
