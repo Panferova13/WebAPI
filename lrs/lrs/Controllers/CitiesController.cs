935aec0639afa20fb155b33e0e0e6b8e8445d748
A.Panferova <panferovaa@icloud.com>
2023-10-27 19:44:28 +0300

    lab6

---
+++ lrs/lrs/Controllers/CitiesController.cs
@@ -0,0 +1,116 @@
+﻿using AutoMapper;
+using Contracts;
+using Entities.DataTransferObjects;
+using Entities.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace lrs.Controllers
+{
+    [Route("api/cities")]
+    [ApiController]
+    public class CitiesController : ControllerBase
+    {
+        private readonly IRepositoryManager _repository;
+        private readonly ILoggerManager _logger;
+        private readonly IMapper _mapper;
+
+        public CitiesController(IRepositoryManager repository, ILoggerManager logger, IMapper mapper)
+        {
+            _repository = repository;
+            _logger = logger;
+            _mapper = mapper;
+        }
+        [HttpGet]
+        public IActionResult GetCities(Guid cityId)
+        {
+            var actionResult = checkResult(cityId);
+            if (actionResult != null)
+                return actionResult;
+            var citiesFromDb = _repository.City.GetCities(cityId, false);
+            var citiesDto = _mapper.Map<IEnumerable<CityDto>>(citiesFromDb);
+            return Ok(citiesDto);
+        }
+        [HttpGet("{id}", Name = "GetCity")]
+        public IActionResult GetCity(Guid cityId, Guid id)
+        {
+            var actionResult = checkResult(cityId);
+            if (actionResult != null)
+                return actionResult;
+            var cityDb = _repository.City.GetCity(id, false);
+            if (cityDb == null)
+            {
+                _logger.LogInfo($"City with id: {id} doesn't exist in the database.");
+                return NotFound();
+            }
+            var city = _mapper.Map<CityDto>(cityDb);
+            return Ok(city);
+        }
+        [HttpPost]
+        public IActionResult CreateCity(Guid cityId, [FromBody] CityCreateDto city)
+        {
+            if (city == null)
+            {
+                _logger.LogError("CityCreateDto object sent from client is null.");
+                return BadRequest("CityCreateDto object is null");
+            }
+            var actionResult = checkResult(cityId);
+            if (actionResult != null)
+                return actionResult;
+            var cityEntity = _mapper.Map<City>(city);
+            _repository.City.CreateCity(cityId, cityEntity);
+            _repository.Save();
+            var cityReturn = _mapper.Map<CityDto>(cityEntity);
+            return CreatedAtRoute("GetCity", new
+            {
+                cityReturn.Id
+            }, cityReturn);
+        }
+        [HttpDelete("{id}")]
+        public IActionResult DeleteCity(Guid cityId, Guid id)
+        {
+            var actionResult = checkResult(cityId);
+            if (actionResult != null)
+                return actionResult;
+            var city = _repository.City.GetCity(id, false);
+            if (city == null)
+            {
+                _logger.LogInfo($"City with id: {id} doesn't exist in the database.");
+                return NotFound();
+            }
+            _repository.City.DeleteCity(city);
+            _repository.Save();
+            return NoContent();
+        }
+        [HttpPut("{id}")]
+        public IActionResult UpdateCity(Guid id, [FromBody] CityUpdateDto city)
+        {
+            if (city == null)
+            {
+                _logger.LogError("CityUpdateDto object sent from client is null.");
+                return BadRequest("CityUpdateDto object is null");
+            }
+            var actionResult = checkResult(id);
+            if (actionResult != null)
+                return actionResult;
+            var cityEntity = _repository.City.GetCity(id, true);
+            if (cityEntity == null)
+            {
+                _logger.LogInfo($"City with id: {id} doesn't exist in the database.");
+                return NotFound();
+            }
+            _mapper.Map(city, cityEntity);
+            _repository.Save();
+            return NoContent();
+        }
+        private IActionResult checkResult(Guid cityId)
+        {
+           
+            {
+                _logger.LogInfo($"Country with id: {cityId} doesn't exist in the database.");
+                return NotFound();
+            }
+            return null;
+        }
+    }
+}
---
+++ lrs/lrs/Controllers/HotelsController.cs
@@ -0,0 +1,120 @@
+﻿using AutoMapper;
+using Contracts;
+using Entities.DataTransferObjects;
+using Entities.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.Sockets;
+
+namespace lrs.Controllers
+{
+    [Route("api/cities/{cityId}/hotels")]
+    [ApiController]
+    public class HotelsController : ControllerBase
+    {
+        private readonly IRepositoryManager _repository;
+        private readonly ILoggerManager _logger;
+        private readonly IMapper _mapper;
+        public HotelsController(IRepositoryManager repository, ILoggerManager logger, IMapper mapper)
+        {
+            _repository = repository;
+            _logger = logger;
+            _mapper = mapper;
+        }
+        [HttpGet]
+        public IActionResult GetHotels(Guid countryId, Guid cityId)
+        {
+            var actionResult = checkResult(cityId);
+            if (actionResult != null)
+                return actionResult;
+            var hotelsFromDb = _repository.Hotel.GetHotels(cityId, false);
+            var hotelsDto = _mapper.Map<IEnumerable<HotelDto>>(hotelsFromDb);
+            return Ok(hotelsDto);
+        }
+        [HttpGet("{id}", Name = "GetHotel")]
+        public IActionResult GetHotel(Guid countryId, Guid cityId, Guid id)
+        {
+            var actionResult = checkResult(cityId);
+            if (actionResult != null)
+                return actionResult;
+            var hotelDb = _repository.Hotel.GetHotel(cityId, id, false);
+            if (hotelDb == null)
+            {
+                _logger.LogInfo($"Hotel with id: {id} doesn't exist in the database.");
+                return NotFound();
+            }
+            var hotel = _mapper.Map<HotelDto>(hotelDb);
+            return Ok(hotel);
+        }
+        [HttpPost]
+        public IActionResult GetHotel(Guid cityId, [FromBody] HotelCreateDto hotel)
+        {
+            if (hotel == null)
+            {
+                _logger.LogError("HotelCreateDto object sent from client is null.");
+                return BadRequest("HotelCreateDto object is null");
+            }
+            var actionResult = checkResult(cityId);
+            if (actionResult != null)
+                return actionResult;
+            var hotelEntity = _mapper.Map<Hotel>(hotel);
+            _repository.Hotel.CreateHotel(cityId, hotelEntity);
+            _repository.Save();
+            var hotelReturn = _mapper.Map<HotelDto>(hotelEntity);
+            return CreatedAtRoute("GetHotel", new
+            {
+
+                cityId,
+                hotelReturn.Id
+            }, hotelReturn);
+        }
+        [HttpDelete("{id}")]
+        public IActionResult DeleteHotel(Guid countryId, Guid cityId, Guid id)
+        {
+            var actionResult = checkResult(cityId);
+            if (actionResult != null)
+                return actionResult;
+            var hotel = _repository.Hotel.GetHotel(cityId, id, false);
+            if (hotel == null)
+            {
+                _logger.LogInfo($"Hotel with id: {id} doesn't exist in the database.");
+                return NotFound();
+            }
+            _repository.Hotel.DeleteHotel(hotel);
+            _repository.Save();
+            return NoContent();
+        }
+        [HttpPut("{id}")]
+        public IActionResult UpdateHotel(Guid countryId, Guid cityId, Guid id, [FromBody] HotelUpdateDto hotel)
+        {
+            if (hotel == null)
+            {
+                _logger.LogError("HotelUpdateDto object sent from client is null.");
+                return BadRequest("HotelUpdateDto object is null");
+            }
+            var actionResult = checkResult(cityId);
+            if (actionResult != null)
+                return actionResult;
+            var hotelEntity = _repository.Hotel.GetHotel(cityId, id, true);
+            if (hotelEntity == null)
+            {
+                _logger.LogInfo($"Hotel with id: {id} doesn't exist in the database.");
+                return NotFound();
+            }
+            _mapper.Map(hotel, hotelEntity);
+            _repository.Save();
+            return NoContent();
+        }
+        private IActionResult checkResult(Guid cityId)
+        {
+           
+            var city = _repository.City.GetCity(cityId, false);
+            if (city == null)
+            {
+                _logger.LogInfo($"City with id: {cityId} doesn't exist in the database.");
+                return NotFound();
+            }
+            return null;
+        }
+    }
+}
