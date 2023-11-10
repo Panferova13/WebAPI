4bfbd968d86539f2edbb50432fd1581185b9d612
Panferova13 <panferovaa@icloud.com>
2023-11-10 20:40:01 +0300

    lab9

---
+++ lrs/lrs/ActionFilters/ValidateHotelExistsAttribute.cs
@@ -0,0 +1,26 @@
+﻿using Contracts;
+using Entities.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace lrs.ActionFilters
+{
+    public class ValidateHotelExistsAttribute : IAsyncActionFilter
+    {
+        private readonly IRepositoryManager _repository;
+        private readonly ILoggerManager _logger;
+        public ValidateHotelExistsAttribute(IRepositoryManager repository, ILoggerManager logger)
+        {
+            _repository = repository;
+            _logger = logger;
+        }
+
+        public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            var method = context.HttpContext.Request.Method;
+            var trackChanges = (method.Equals("PUT") || method.Equals("PATCH")) ? true : false;
+            var partWorldId = (Guid)context.ActionArguments["partWorldId"];
+!    
+        }
+    }
+}
