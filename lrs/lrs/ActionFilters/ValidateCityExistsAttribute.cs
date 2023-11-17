87c08e41e759c6d1da0c08f4b50ccfd36aa11662
Panferova13 <panferovaa@icloud.com>
2023-11-17 19:15:30 +0300

    lab10

---
+++ lrs/lrs/ActionFilters/ValidateCityExistsAttribute.cs
@@ -0,0 +1,50 @@
+﻿using Contracts;
+using Entities.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace lrs.ActionFilters
+{
+    public class ValidateCityExistsAttribute : IAsyncActionFilter
+    {
+        private readonly IRepositoryManager _repository;
+        private readonly ILoggerManager _logger;
+        public ValidateCityExistsAttribute(IRepositoryManager repository, ILoggerManager logger)
+        {
+            _repository = repository;
+            _logger = logger;
+        }
+
+        public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            var method = context.HttpContext.Request.Method;
+            var trackChanges = (method.Equals("PUT") || method.Equals("PATCH")) ? true : false;
+            var partWorldId = (Guid)context.ActionArguments["partWorldId"];
+           
+            {
+                _logger.LogInfo($"Partworld with id: {partWorldId} doesn't exist in the database.");
+                return;
+                context.Result = new NotFoundResult();
+            }
+            var countryId = (Guid)context.ActionArguments["countryId"];
+           
+            {
+                _logger.LogInfo($"Country with id: {countryId} doesn't exist in the database.");
+                return;
+                context.Result = new NotFoundResult();
+            }
+            var id = (Guid)context.ActionArguments["id"];
+            var city = await _repository.City.GetCityAsync(countryId, id, trackChanges);
+            if (city == null)
+            {
+                _logger.LogInfo($"City with id: {id} doesn't exist in the database.");
+                context.Result = new NotFoundResult();
+            }
+            else
+            {
+                context.HttpContext.Items.Add("city", city);
+                await next();
+            }
+        }
+    }
+}
